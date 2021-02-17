3376aafca431c6142272323296a4715300f2d820
okaytrev <78512143+okaytrev@users.noreply.github.com>
2021-02-17 05:49:17 -0700

    Add files via upload

--- AzureAdAutomation.ps1
+++ AzureAdAutomation.ps1
@@ -1,21 +1,26 @@
 ﻿###Azure AD bulk creation by CSV
 ###By: Trevar Hupf
-###Requires AzureAD module to be installed - http://mosshowto.blogspot.com/2019/08/create-user-azure-ad-powershell.html
-###Requires connect-AzureAd to desired tenant 
+###Require AzureAD module to be installed - http://mosshowto.blogspot.com/2019/08/create-user-azure-ad-powershell.html
+###Require connect-AzureAd to desired tenant 
+
+#sets logpath to current directory
+$LogPath = $PSScriptRoot + "\log.txt"
+
+#Logs date before each run
+$currentdate = Get-Date 
+Write-Output "--------This script ran on $currentdate ---------" | Out-File -Append -FilePath $LogPath
 
 # Create a password object
 $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
 
 #Store the data from ADUsers.csv in the $ADUsers variable 
-$ADUsers = Import-csv C:\Users\trevar.hupf\Desktop\test.csv
+$ADUsers = Import-csv "$PSScriptRoot\newusers.csv"
 
 #Loop through each row containing user details in the CSV file 
 foreach ($User in $ADUsers)
 {
 	#Read user data from each field in each row and assign the data to a variable as below	
 	$displayname = $User.displayname
-	$firstname 	= $User.firstname
-	$lastname 	= $User.lastname
 	$jobtitle 	= $User.jobtitle
 	$department = $User.department
     $mailnickname     = $User.mailnickname
@@ -25,16 +30,18 @@ foreach ($User in $ADUsers)
 	 if (Get-AzureADUser -Filter "DisplayName eq '$displayname'")
 	{
 		 #If user does exist, give a warning
-		 Write-Warning "A user account with username $Username already exist in Azure AD"
+		 Write-Warning "A user account with username $Displayname already exist in Azure AD"
+		 $displayname + " ********ALREADY EXISTS" | Out-File -Append -FilePath $LogPath
 	}
 	else
 	{
-		#Proceeds if user does not exists
+		#Proceeds if user does not exist
         $fullmailnickname = "$mailnickname" + "@trevdomain.onmicrosoft.com"
         write-host = $fullmailnickname
 		
         #User creation
 		New-AzureadUser -AccountEnabled $true -DisplayName $displayname -UserPrincipalName $fullmailnickname  -Jobtitle $jobtitle -department $department -MailNickName $mailnickname -PasswordProfile $PasswordProfile 
             
+		$displayname | Out-File -Append -FilePath $LogPath
 	}
 }
